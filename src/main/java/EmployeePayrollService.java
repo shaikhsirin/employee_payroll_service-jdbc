1c08b5cd5c10c94212c9fe7124cc27ec70d2b46f
shaikhsirin <shaikhsirin8080example.com>
2022-04-18 16:32:48 +0530

    Added:-Retrive Employee Payroll Data From DataTable

---
+++ src/main/java/EmployeePayrollService.java
@@ -0,0 +1,86 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+/**
+ * Create class EmployeePayrollService
+ *
+ * @author Sirin
+ *
+ */
+public class EmployeePayrollService {
+    public enum IOService {
+        CONSOLE_IO, FILE_IO, DB_IO, REST_IO;
+
+        public static void printData() {
+        }
+    }
+
+    /**
+     * Create a ArryList EmployeePayrollData create Scanner object
+     */
+    public static final Scanner SC = new Scanner(System.in);
+    private List<EmployeePayrollData> employeePayrollList;
+
+    public EmployeePayrollService() {
+        this.employeePayrollList = new ArrayList<EmployeePayrollData>();
+    }
+
+    public EmployeePayrollService(List<EmployeePayrollData> employeeList) {
+        this.employeePayrollList = employeeList;
+    }
+
+    public int sizeOfEmployeeList() {
+        return this.employeePayrollList.size();
+    }
+
+    public void readEmployeeData(IOService ioType) {
+        if (ioType.equals(IOService.CONSOLE_IO)) {
+            System.out.println("Enter employee id:");
+            int employeeId = SC.nextInt();
+            System.out.println("Enter employee name:");
+            SC.nextLine();
+            String employeeName = SC.nextLine();
+            System.out.println("Enter employee salary:");
+            double employeeSalary = SC.nextDouble();
+            EmployeePayrollData newEmployee = new EmployeePayrollData(employeeId, employeeName, employeeSalary);
+            employeePayrollList.add(newEmployee);
+        } else if (ioType.equals(IOService.FILE_IO))
+            this.employeePayrollList = new FileIOService().readData();
+        else if (ioType.equals(IOService.DB_IO)) {
+            try {
+                this.employeePayrollList = new DataBaselIOService().readData();
+            } catch (DBException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public void writeEmployeeData(IOService ioType) {
+        if (ioType.equals(IOService.CONSOLE_IO)) {
+            for (EmployeePayrollData o : employeePayrollList)
+                System.out.println(o.toString());
+        } else if (ioType.equals(IOService.FILE_IO)) {
+            new FileIOService().writeData(employeePayrollList);
+        }
+    }
+
+    public long countEnteries(IOService ioType) {
+        if (ioType.equals(IOService.FILE_IO))
+            return new FileIOService().countEntries();
+        return 0;
+    }
+
+    public List<EmployeePayrollData> printEmployeePayrollData(IOService ioType) {
+        if (ioType.equals(IOService.FILE_IO))
+            new FileIOService().printEmployeePayrolls();
+        else
+            this.employeePayrollList.stream().forEach(employeeData -> System.out.println(employeeData.toString()));
+        return employeePayrollList;
+    }
+
+    public void printData(IOService fileIo) {
+
+    }
+}
+
---
+++ src/test/java/EmployeePayrollServiceTest.java
@@ -0,0 +1,45 @@
+import static java.util.Arrays.asList;
+
+import java.util.List;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+/**
+ * Create class EmployeePayrollServiceTest Added Salary is Updated Database
+ */
+
+public class EmployeePayrollServiceTest {
+    private static final EmployeePayrollService.IOService DB_IO = null;
+    @Test
+    public void given3EmployeeWhenWrittenToFileShouldMatchEmployeeEntries() {
+        EmployeePayrollData[] arrayOfEmps = { new EmployeePayrollData(1, "Jeff Bezos", 100000.0),
+                new EmployeePayrollData(2, "Bill Gates", 200000.0),
+                new EmployeePayrollData(3, "Mark Zuckerberg", 300000.0) };
+        EmployeePayrollService employeePayrollService;
+        employeePayrollService = new EmployeePayrollService(asList(arrayOfEmps));
+        employeePayrollService.printEmployeePayrollData(EmployeePayrollService.IOService.FILE_IO);
+        EmployeePayrollService.IOService.printData();
+        long entries = employeePayrollService.countEnteries(EmployeePayrollService.IOService.FILE_IO);
+        Assertions.assertEquals(3, entries);
+    }
+
+
+    /**
+     * Create method givenFileOnReadingFromFileShouldMatchEmployeeCount create
+     * object of EmployeePayrollService Create Array List of EmployeePayrollData
+     */
+    @Test
+    public void givenFileOnReadingFromFileShouldMatchEmployeeCount() {
+        EmployeePayrollService employeePayrollService = new EmployeePayrollService();
+        List<EmployeePayrollData> entries = employeePayrollService
+                .printEmployeePayrollData(EmployeePayrollService.IOService.FILE_IO);
+        Assertions.assertEquals(3, entries);
+    }
+
+    @Test
+    public void givenEmployeePayrollInDB_WhenRetrived_ShouldMathchEmployeeCount() {
+        EmployeePayrollService employeePayrollService = new EmployeePayrollService();
+        List<EmployeePayrollData> employeePayrollData = employeePayrollService.printEmployeePayrollData(DB_IO);
+        Assertions.assertEquals(3, employeePayrollData.size());
+    }
+}
